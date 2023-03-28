70ee974d2ff7b6ec1eaab5ce34f096d57e56216e
Ariel Vardy <77558607+ArJonVar@users.noreply.github.com>
2023-03-28 13:52:12 -0700

    successful post

--- cron_fw_update.py
+++ cron_fw_update.py
@@ -5,9 +5,9 @@ from smartsheet_grid import grid
 import requests
 import json
 import time
-from globals import smartsheet_token, bamb_api_key, fw_api_key
+from globals import smartsheet_token, fw_api_key
 from logger import ghetto_logger
-import datetime
+from datetime import datetime, date
 
 #endregion
 
@@ -15,14 +15,14 @@ class FwApi():
     '''is meant to work with Safety Mat's Safety Project List to find Active Construction Projects and update the dates that project employees have completed their trainings to have a pulse on the level of training of project teams.'''
     def __init__(self, ss_api_token, fw_api_token):
         self.smartsheet_token=ss_api_token
-        self.fw_token=fw_api_key
+        self.fw_token=fw_api_token
         grid.token=smartsheet_token
         self.smart = smartsheet.Smartsheet(access_token=self.smartsheet_token)
         self.smart.errors_as_exceptions(True)
-        self.safety_pl_sheet_id = 8139053347432324
-        self.update_stamp_sum_id = 'get correct id'
+        self.safety_pl_sheet_id = 4916349521684356
+        self.update_stamp_sum_id = 790362046392196
         self.start_time = time.time()
-        self.log=ghetto_logger("cron_taining_update.py")
+        self.log=ghetto_logger("cron_fw_update.py")
     def timestamp(self): 
         '''creates a string of minute/second from start_time until now for logging'''
         end_time = time.time()  # get the end time of the program
@@ -38,7 +38,8 @@ class FwApi():
         sheet = grid(sheet_id)
         sheet.fetch_content()
         self.sheet_columns = sheet.get_column_df()
-        self.log.log(f"{self.timestamp()} Successfully connected to Smartsheet")
+        self.locate_posting_column_ids()
+        self.log.log(f"Successfully connected to Smartsheet")
         return sheet.df
     def clean_smartsheet_data(self, safety_df):
         '''filters the data for the parameters we need, and then only returns columns we need'''
@@ -65,14 +66,23 @@ class FwApi():
                     # this is always where the id is int he url
                     id = item.get("FW")[38:74]
                 except:
-                    print(f"FW link for {item.get('FW')} is incorrect and id could not be extrapolated")
+                    self.log.log(f"FW link for {item.get('FW')} is incorrect and id could not be extrapolated")
                 item["fw_id"] = id
             else:
                 item["fw_id"] = "None"
         return data_list
     def locate_posting_column_ids(self):
         '''Generates the Column id's based on column name in case we need to switch sheets for what ever reason'''
-        pass
+        df = self.sheet_columns
+        self.count_joblog_columnid = df.loc[df['title'] == 'Count Daily Job Logs']['id'].to_list()[0]
+        self.recent_joblog_columnid = df.loc[df['title'] == 'Most Recent Daily Job Log']['id'].to_list()[0]
+        self.count_safetymeeting_columnid = df.loc[df['title'] == 'Count Weekly Safety Meetings']['id'].to_list()[0]
+        self.recent_safetymeeting_columnid = df.loc[df['title'] == 'Most Recent Weekly Safety Meeting']['id'].to_list()[0]
+        self.count_safetyinspections_columnid = df.loc[df['title'] == 'Count Safety Inspections']['id'].to_list()[0]
+        self.recent_safetyinspections_columnid = df.loc[df['title'] == 'Most Recent Safety Inspection']['id'].to_list()[0]
+        self.recent_geninfo_columnid = df.loc[df['title'] == 'SSSP - General Information']['id'].to_list()[0]
+        self.count_premob_columnid = df.loc[df['title'] == 'Total Count SSSP - Pre-Mob Sub Checklists']['id'].to_list()[0]
+        self.count_phasereview_columnid = df.loc[df['title'] == 'Total Count SSSP - Phase Reviews']['id'].to_list()[0]
 #endregion
 #region fw data
     def fw_api_call(self, url):
@@ -122,7 +132,7 @@ class FwApi():
         response = requests.get(url, headers=headers)
         content = json.loads(response.content)
         return content
-    def fetch_fw_data(self, id, url):
+    def fetch_fw_data(self, id, url, name):
         '''manages fw api calls, and fails them at once if needed'''
         try:
             status_dict=self.get_statuses(id)
@@ -130,14 +140,14 @@ class FwApi():
             forms_data = self.get_all_forms(id)
             return {"status_dict":status_dict, "template_dict":template_dict, "forms_data":forms_data}
         except AttributeError:
-            print(f"error w/ {id} at {url}")
+            self.log.log(f"Need access to {name}: {url}")
             return{"forms_data":[]}
     def integrate_fw_data(self, data):
         '''integrates all fw data, mapping ids to text (for template/status) and integrating all data with the record per proj'''
         for item in data:
             id = item.get("fw_id")
             if id != "None":
-                fw_data = self.fetch_fw_data(id, item.get("FW"))
+                fw_data = self.fetch_fw_data(id, item.get("FW"), item.get("NAME"))
                 
                 forms = []
                 for form in fw_data.get("forms_data"):
@@ -157,7 +167,6 @@ class FwApi():
 
         return data
 #endregion
-
 #region post data
     def calc_meta_data(self, forms, template):
         '''returns count and more recent for various templates'''
@@ -166,7 +175,6 @@ class FwApi():
             # filters for Daily Job Log template w/ completed/submitted status
             if form.get('template').find(template) != -1 and (form.get('status').find("Complete") != -1 or form.get('status').find("Submitted") != -1):
                     filtered_data.append(form)
-        
         try:
             count = len(filtered_data)
         except:
@@ -177,40 +185,81 @@ class FwApi():
             most_recent_date = "N/A"
 
         return count, most_recent_date
+    def date_parser(self, input_str):
+        '''changes the date from the format it gets in fw {'2023-03-27T19:21:51.879Z'} to the format easiest on the eyes {%m/%d/%y}'''
+        if input_str != "N/A":
+            # Parse the input string using datetime.strptime
+            dt = datetime.strptime(input_str, '%Y-%m-%dT%H:%M:%S.%fZ')
+            # Format the datetime object as a string in the desired format
+            formatted_date = dt.strftime('%Y-%m-%d')
+            # Return the formatted string
+            return formatted_date
+        else:
+            return ""
+    def pull_main_data(self, item):
+        '''the main data pull, which grabs all needed data, and formats it correct'''
+        count_daily_joblog,recent_daily_joblog = self.calc_meta_data(item.get('forms'), "Daily Job Log")
+        count_weekly_safetymeeting, recent_weekly_safetymeeting=self.calc_meta_data(item.get('forms'), "Weekly Site Safety Meeting")
+        count_safety_inspections, recent_safety_inspections = self.calc_meta_data(item.get('forms'), "Weekly Site Safety Inspection")
+        count_sssp, recent_sssp = self.calc_meta_data(item.get('forms'), "General Project Info")
+        count_premob, recent_premob= self.calc_meta_data(item.get('forms'), "Pre-mob Sub Checklist")
+        count_phase_review, recent_phase_review = self.calc_meta_data(item.get('forms'), "Project Phase Review")
+        post = [{"name":"count_daily_joblog", "value":count_daily_joblog, "column_id": self.count_joblog_columnid },
+                {"name":"recent_daily_joblog", "value": self.date_parser(recent_daily_joblog), "column_id":self.recent_joblog_columnid},
+                {"name":'count_weekly_safetymeeting', "value":count_weekly_safetymeeting, "column_id":self.count_safetymeeting_columnid},
+                {"name":'recent_weekly_safetymeeting', "value":self.date_parser(recent_weekly_safetymeeting), "column_id":self.recent_safetymeeting_columnid},
+                {"name":'count_safety_inspections', "value":count_safety_inspections, "column_id":self.count_safetyinspections_columnid},
+                {"name":'recent_safety_inspections', "value":self.date_parser(recent_safety_inspections), "column_id":self.recent_safetyinspections_columnid},
+                {"name":'recent_sssp',  "value":self.date_parser(recent_sssp), "column_id":self.recent_geninfo_columnid},
+                {'name':'count_premob', "value":count_premob, "column_id":self.count_premob_columnid},
+                {"name":'count_phase_review', "value":count_phase_review, "column_id":self.count_phasereview_columnid}
+            ]
+        item["post"]=post
+        
+        return item
     def gather_posting_data(self, data):
-        '''to gather posting data we need to iterate through row data and find employee ids, and then find each employees trainings. 
-        If there is no employee, the value can be "N/A" if an employee is missing a required training the value can be "", and if they have completed it, the value will be their completion date.'''
+        '''to gather posting data we need to iterate through row data and for each row, gather the appropropriate data to prep for posting.'''
         self.error = []
         for item in data:
             id = item.get("fw_id")
             forms = item.get("forms")
             if id != "None" and forms != []:
                 try:
-                    count_daily_joblog,recent_daily_joblog = self.calc_meta_data(item.get('forms'), "Daily Job Log")
-                    count_weekly_safetymeeting, recent_weekly_safetymeeting=self.calc_meta_data(item.get('forms'), "Weekly Site Safety Meeting")
-                    count_safety_inspections, recent_safety_inspections = self.calc_meta_data(item.get('forms'), "Weekly Site Safety Inspection")
-                    count_sssp, recent_sssp = self.calc_meta_data(item.get('forms'), "General Project Info")
-                    count_premob, recent_premob= self.calc_meta_data(item.get('forms'), "Pre-mob Sub Checklist")
-                    count_phase_review, recent_phase_review = self.calc_meta_data(item.get('forms'), "Project Phase Review")
-                    post = {"count_daily_joblog": count_daily_joblog, 
-                            "recent_daily_joblog": recent_daily_joblog, 
-                            'count_weekly_safetymeeting':count_weekly_safetymeeting,
-                            'recent_weekly_safetymeeting':recent_weekly_safetymeeting,
-                            'count_safety_inspections': count_safety_inspections,
-                            'recent_safety_inspections': recent_safety_inspections,
-                            'recent_sssp': recent_sssp,
-                            'count_premob': count_premob,
-                            'count_phase_review': count_phase_review
-                            }
-                    item["post"]=post
+                    self.pull_main_data(item)
                 except:
                     self.error.append(item)
+        
+        self.log.log(f"the following {len(self.error)} projects produced an error:")
+        for error in self.error:
+            self.log.log(f"  {error.get('NAME')}: {error.get('FW')}")
+        
         return data
     def post_data(self, posting_data):
-        pass
+        '''posting to ss'''
+        self.log.log(f"posting...")
+        self.row_data = []
+        for i, row in enumerate(posting_data):
+            if row.get("post") != None:
+                new_row = smartsheet.models.Row()
+                new_row.id = int(row.get("id"))
+                for column in row.get("post"):
+                    column_id = column.get("column_id")
+                    value = column.get("value")
+                    new_cell = smartsheet.models.Cell()
+                    new_cell.column_id = int(column_id)
+                    new_cell.value = value
+                    new_cell.strict = False
+                    new_row.cells.append(new_cell)
+                    if new_row not in self.row_data:
+                        self.row_data.append(new_row)
+        resp = self.smart.Sheets.update_rows(
+            int(self.safety_pl_sheet_id),      # sheet_id
+            self.row_data)
+        
+        self.log.log(resp.message)
     def post_update_stamp(self):
         '''posts date to summary column to tell ppl when the last time this script succeeded was'''
-        current_date = datetime.date.today()
+        current_date = date.today()
         formatted_date = current_date.strftime('%m/%d/%y')
 
         sum = smartsheet.models.SummaryField({
@@ -225,13 +274,33 @@ class FwApi():
         )
 #endregion
     def run(self):
+        self.log.log("pulling ss data")
         df = self.gather_smartsheet_data(self.safety_pl_sheet_id)
         self.df_reordered = self.clean_smartsheet_data(df)
         data = self.gen_ss_data_list(self.df_reordered)
+        self.log.log("pulling fw data")
         data=self.integrate_fw_data(data)
         self.posting_data = self.gather_posting_data(data)
+        self.log.log("posting data")
+        self.post_data(self.posting_data)
+        self.post_update_stamp()
+        self.log.log("~fin")
+
 
 
 if __name__ == "__main__":
     fa = FwApi(smartsheet_token, fw_api_key)
     fa.run()
+
+
+
+# [{"name":"count_daily_joblog", "value":count_daily_joblog, "column_id": "734374284552068"},
+#                             {"name":"recent_daily_joblog", "value": self.date_parser(recent_daily_joblog), "column_id":"8052723679029124"},
+#                             {"name":'count_weekly_safetymeeting', "value":count_weekly_safetymeeting, "column_id":"5562574521558916"},
+#                             {"name":'recent_weekly_safetymeeting', "value":self.date_parser(recent_weekly_safetymeeting), "column_id":"1015869528663940"},
+#                             {"name":'count_safety_inspections', "value":count_safety_inspections, "column_id":"3310774707873668"},
+#                             {"name":'recent_safety_inspections', "value":self.date_parser(recent_safety_inspections), "column_id":"5519469156034436"},
+#                             {"name":'recent_sssp',  "value":self.date_parser(recent_sssp), "column_id":"2184874801031044"},
+#                             {'name':'count_premob', "value":count_premob, "column_id":"7814374335244164"},
+#                             {"name":'count_phase_review', "value":count_phase_review, "column_id":"6688474428401540"}
+#                         ]
